7a4753ce29d4ca8588658dda5a7a414f4adea5dc
muthuluri-janaki.muthuluri-janaki <you@example.com>
2026-01-30 19:40:18 +0530

    Ignore lambda zip file

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -79,10 +79,10 @@ resource "aws_lambda_function" "etl_lambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
-  # filename      = "${path.module}/etl_lambda.zip"
+  filename      = "${path.module}/etl_lambda.zip"
   # source_code_hash = filebase64sha256("${path.module}/etl_lambda.zip")
-  filename = data.archive_file.etl_lambda_zip.output_path
-  source_code_hash = data.archive_file.etl_lambda_zip.output_base64sha256
+  # filename = data.archive_file.etl_lambda_zip.output_path
+  # source_code_hash = data.archive_file.etl_lambda_zip.output_base64sha256
   timeout = 300
   memory_size = 512
  
