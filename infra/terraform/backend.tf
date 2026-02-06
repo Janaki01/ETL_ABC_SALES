c972cce78cecf94d556701c3678ea7873891a071
muthuluri-janaki.muthuluri-janaki <you@example.com>
2026-02-06 10:19:50 +0530

    added backend

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,10 +1,9 @@
-
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-janaki"
-#     key = "etl/terraform.tfstate"
-#     region = "ap-south-1"
-#     dynamodb_table = "terraform-locks"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "etl-terraform-states-bucket"
+    key = "etl/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
+}
